75d470b0911d9013be6060b452c4d730e5561364
Cory Johnson <cory1993johnson@gmail.com>
2024-05-05 15:48:42 +0200

    comment update

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@ use td_foo::AppPlugin;
 
 // TODO: add mouse events/mouse tracker
 
+/* --- Developer Info ---
+
+    * Controls
+        M -> Main Menu
+        G -> Game
+        P -> toggle Game Paused and Running
+        S -> Spawn Tank at starting pos
+        N -> Move Tank forward
+ */
+
 fn main() {
     App::new()
         .add_plugins(AppPlugin)
